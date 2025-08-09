0dfb7567b558ff52d84a5725fcc65136eadebb31
SK MD RAMIJ IKBAL <ramijikbal839@gmail.com>
2025-08-09 11:40:22 +0530

    Add files via upload

---
+++ vs_code/dsa/Java Program/ArrayList/pair_sum_1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class pair_sum_1 {
+    public static boolean pair_sum(ArrayList<Integer> list, int target) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+    
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 5;
+        System.out.println(pair_sum(list, target));
+    }
+}
